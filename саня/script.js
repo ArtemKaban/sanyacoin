1462b8787bbac90575f796d62cff35ae0f882ad3
ArtemKaban <artmekabansky001@gmail.com>
2024-08-23 19:34:11 +0300

    Delete саня directory

--- саня/script.js
+++
@@ -1,8 +0,0 @@
-var san = document.getElementById('san');
-var p = document.getElementById('s');
-var counter = 0;
-san.addEventListener("click",function()
-{
-    counter++;
-    p.textContent = counter;
-});
